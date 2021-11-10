7f302f397b6009a3e0700bf05a0a8f6c520e4bad
githubyunhan <240791094@qq.com>
2021-11-10 20:33:08 +0800

    1 Spring Boot Start

---
+++ src/test/java/cn/itcast/springbootstudy/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itcast.springbootstudy;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
