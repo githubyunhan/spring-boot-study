b2e6eb4d717c28c68fd1cda6c2f656a1c9a58133
githubyunhan <240791094@qq.com>
2021-11-18 11:36:41 +0800

    13.2 全局异常处理ExceptionHandler

--- src/main/java/cn/itcast/springbootstudy/controller/HelloController.java
+++ src/main/java/cn/itcast/springbootstudy/controller/HelloController.java
@@ -1,13 +1,20 @@
 package cn.itcast.springbootstudy.controller;
 
+import cn.itcast.springbootstudy.config.exception.AjaxResponse;
 import cn.itcast.springbootstudy.model.ArticleVO;
+import cn.itcast.springbootstudy.service.ExceptionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @Slf4j
 @RestController
 public class HelloController {
+    @Resource
+    ExceptionService exceptionService;
 
     @RequestMapping("/hello")
     public ArticleVO hello(){
@@ -16,5 +23,16 @@ public class HelloController {
         ArticleVO article1= ArticleVO.builder().id(3L).author("莫言").build();
         log.info("测试一下"+article1);
         return article1;
+    }
+
+    @RequestMapping("/ex/system")
+    public @ResponseBody AjaxResponse system(){
+        exceptionService.systemBizError();
+        return AjaxResponse.success();
+    }
+
+    @RequestMapping("/ex/user")
+    public @ResponseBody AjaxResponse user(Integer input){
+        return AjaxResponse.success(exceptionService.userBizError(input));
     }
 }
