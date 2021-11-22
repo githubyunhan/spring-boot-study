62006987cae142804ed10ea4086d48ed471e8351
githubyunhan <240791094@qq.com>
2021-11-22 15:36:05 +0800

    14.3 拦截器实现统一访问日志

---
+++ src/main/java/cn/itcast/springbootstudy/config/MyWebMvcConfigurer.java
@@ -0,0 +1,20 @@
+package cn.itcast.springbootstudy.config;
+
+import cn.itcast.springbootstudy.config.accesslog.AccessLogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {/*③拦截器注册*/
+
+    //设置排除路径，spring boot 2.*，注意排除掉静态资源的路径，不然静态资源无法访问
+    private final String[] excludePath = {"/static"};
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //注册拦截器 拦截规则
+        //多个拦截器时 以此添加 执行顺序按添加顺序
+        registry.addInterceptor(new AccessLogInterceptor()).addPathPatterns("/**").excludePathPatterns(excludePath);
+    }
+}
