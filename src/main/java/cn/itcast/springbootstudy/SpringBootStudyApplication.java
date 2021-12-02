6d42a03c07d685ee6db408137cdd710f55e7cd54
githubyunhan <240791094@qq.com>
2021-12-02 21:07:25 +0800

    16.6 集群多节点应用session共享

--- src/main/java/cn/itcast/springbootstudy/SpringBootStudyApplication.java
+++ src/main/java/cn/itcast/springbootstudy/SpringBootStudyApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @ImportResource(locations = {"classpath:beans.xml"})//导入外部配置文件，.xml文件
 @MapperScan(basePackages = {"cn.itcast.springbootstudy.generator"})
 @EnableCaching/*开启缓存*/
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 30 * 60 * 1000)/*Session有效期是30分钟在启动类上加上该注解：配置启用redis的httpSession*/
 public class SpringBootStudyApplication {
 
     public static void main(String[] args) {
